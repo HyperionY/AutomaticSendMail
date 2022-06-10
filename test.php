b1a1e7d429abbc997573cfd30647cc90c5a0c417
HyperionY <yjyj989812@naver.com>
2022-06-10 21:31:11 +0900

    Functioning automatic send mail

--- test.php
+++ test.php
@@ -8,65 +8,32 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
-//Create a new PHPMailer instance
-$mail = new PHPMailer(true);
+function automaticMailer($address, $pw, $to) {
+    $mail = new PHPMailer(true);
+    $mail->isSMTP();
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;
 
-//Tell PHPMailer to use SMTP
-$mail->isSMTP();
-
-//Enable SMTP debugging
-//SMTP::DEBUG_OFF = off (for production use)
-//SMTP::DEBUG_CLIENT = client messages
-//SMTP::DEBUG_SERVER = client and server messages
-$mail->SMTPDebug = SMTP::DEBUG_SERVER;
-
-try {
-    //Set the hostname of the mail server
     $mail->Host = 'smtp.gmail.com';
     $mail->SMTPAuth = true;
-
-    //Set the SMTP port number:
-    // - 465 for SMTP with implicit TLS, a.k.a. RFC8314 SMTPS or
-    // - 587 for SMTP+STARTTLS
     $mail->Port = 465;
-
-    //Set the encryption mechanism to use:
-    // - SMTPS (implicit TLS on port 465) or
-    // - STARTTLS (explicit TLS on port 587)
     $mail->SMTPSecure = "ssl";
 
-    //Username to use for SMTP authentication - use full email address for gmail
-    $mail->Username = 'your@email';
-
-    //Password to use for SMTP authentication
-    $mail->Password = 'your_pw';
+    $mail->Username = $address;
+    $mail->Password = $pw;
 
     $mail->CharSet = 'utf-8';
     $mail->Encoding = "base64";
 
-    //Set who the message is to be sent from
-    //Note that with gmail you can only use your account address (same as `Username`)
-    //or predefined aliases that you have configured within your account.
-    //Do not use user-submitted addresses in here
-    $mail->setFrom('your_@email', 'Tester');
-
-    //Set who the message is to be sent to
-    $mail->addAddress('address@email', 'who');
+    $mail->setFrom($address, 'Remainder');
+    $mail->addAddress($address, $to);
 
     $mail->isHTML(true);
 
-    //Set the subject line
     $mail->Subject = 'PHPMailer GMail SMTP test SUBJECT';
-
-    //Replace the plain text body with one created manually
     $mail->Body = 'This is a test message body';
     $mail->AltBody = 'This is a plain-text message body test';
-    
+
     $mail->Send();
     echo "Message has heen sent";
-} catch (phpmailerException $e) {
-    echo $e->errorMessage();
-} catch (Exception $e) {
-    echo $e->getMessage();
 }
 ?>
