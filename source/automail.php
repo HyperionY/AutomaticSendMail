2d4c91aa3f30952b01d4bee5ec009469c14f7456
HyperionY <yjyj989812@naver.com>
2022-06-11 00:40:43 +0900

    add coment to automail.php and autosend_routine.php

--- source/automail.php
+++ source/automail.php
@@ -1,30 +1,55 @@
 <?php
+// refer to phpmailer lib.
 require '../vendor/autoload.php';
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
+/*
+  automaticMailer( $address :: "Your email address for sending.",
+                   $pw      :: "Your email account password.",
+                   $to      :: "Your account username.",
+                   $type    :: "Parameters that determine which content to mail."
+                                0 == start program.
+                                1 == send email when morning.
+                                2 == send email when activity time.
+                                3 == send email when get to sleep.)
+*/
 function automaticMailer($address, $pw, $to, $type=0) {
+    // defaults to using php "mail()" and SMTP sever setting.
+    // if you need to debug use '$mail->SMTPDebug'
     $mail = new PHPMailer(true);
     $mail->isSMTP();
     //$mail->SMTPDebug = SMTP::DEBUG_SERVER;
 
+    // set the google SMTP setting.
+    // if you use a different email, please set SMTP settings for that email.
     $mail->Host = 'smtp.gmail.com';
     $mail->SMTPAuth = true;
     $mail->Port = 465;
     $mail->SMTPSecure = "ssl";
 
+    // set the email address and password for send email.
     $mail->Username = $address;
     $mail->Password = $pw;
 
+    // encording set
     $mail->CharSet = 'utf-8';
     $mail->Encoding = "base64";
 
+    // set mail sneder and receiver.
     $mail->setFrom($address, 'Remainder');
     $mail->addAddress($address, $to);
+
+    // set content type.
     $mail->isHTML(true);
 
+    /*
+      $mail->Subject :: mail title
+      $mail->Body    :: mail content (HTML only)
+      $mail->AltBody :: mail content (non-HTML only)
+    */
     if($type === 0)
     {
         $mail->Subject = 'Automaic send remainder mail program start';
--- source/autosend_routine.php
+++ source/autosend_routine.php
@@ -1,38 +1,46 @@
 <?php
+// refer to automail.php for send mail function.
 include_once('./automail.php');
 
+// set the timezone
+// if you want to use other timezone, change 'Asia/Seoul'.
 date_default_timezone_set('Asia/Seoul');
 
-$o_c = ['08', '23'];
+$o_c = ['08', '23'];    // get up time and go to sleep time
+//time to send mail for remainder.
+// if you want to change send mail time, edit time_set array.
 $time_set = ['10', '11', '14', '17', '20'];
 
 echo "test start!\n";
 
+// get program start time
 $timestamp = strtotime("Now");
 $current_hour = date("H", $timestamp);
-automaticMailer("your@email", "your_password", "your_name");
+
+// send mail when start program
+automaticMailer("yourAddress@email.com", "your_password", "your_username"); 
 
 echo "Program start time: ".date("Y-m-d H:i:s", $timestamp)."<br/>";
 echo "current hour: ".date("H", $timestamp)."<br/>";
 
 while(true) 
 {
-    $timestamp = strtotime("Now");
+    $timestamp = strtotime("Now");     // get time for compare time
     $target_hour = date("H", $timestamp);
 
     if($current_hour != $target_hour) 
     {
         if($target_hour === $o_c[0]) 
         {
-            automaticMailer("your@email", "your_password", "your_name", 1);
-            $current_hour = date("H", $timestamp);
+            automaticMailer("yourAddress@email.com", "your_password", "your_username", 1);   // send mail when get up
+            $current_hour = date("H", $timestamp);  //reset the current time
 
             echo "Send time: ".date("Y-m-d H:i:s", $timestamp)."<br/>";
             echo "Send morning mail\n";
         } 
         else if ($target_hour === $o_c[1]) 
         {
-            automaticMailer("your@email", "your_password", "your_name", 3);
+            automaticMailer("yourAddress@email.com", "your_password", "your_username", 3);   // send mail when get to sleep
             $current_hour = date("H", $timestamp);
 
             echo "Send time: ".date("Y-m-d H:i:s", $timestamp)."<br/>";
@@ -44,7 +52,7 @@ while(true)
             {
                 if($current_hour === $time_set[$i]) 
                 {
-                    automaticMailer("your@email", "your_password", "your_name", 2);
+                    automaticMailer("yourAddress@email.com", "your_password", "your_username", 2);   // send mail during work for reminder
                     $current_hour = date("H", $timestamp);
 
                     echo "Send time: ".date("Y-m-d H:i:s", $timestamp)."<br/>";
@@ -55,7 +63,7 @@ while(true)
         }
     }
 
-    sleep(600);
+    sleep(600); //delay 10min
 }
 
 ?>
